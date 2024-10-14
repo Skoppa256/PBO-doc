15c4713c4e9295cd5b05b15db5805799eb7273d7
Skoppa256 <73915593+Skoppa256@users.noreply.github.com>
2024-10-14 21:07:56 +0800

    Update SupportSystem.java

--- W6/src/SupportSystem.java
+++ W6/src/SupportSystem.java
@@ -17,7 +17,7 @@ public class SupportSystem {
                 finished = true;
             }
             else {
-                String response = responder.generateResponse(); System.out.println(response);
+                String response = responder.generateResponse(input); System.out.println(response);
             }
         }
 
